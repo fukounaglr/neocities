8076254ef981c85b68660294945fc38acfb6f819
Sasha J. Neil <140499150+ashhmillr@users.noreply.github.com>
2024-03-10 16:00:12 +0300

    Add files via upload

--- js/drag.js
+++ js/drag.js
@@ -1,42 +1,30 @@
-dragElement(document.getElementById("sticker1"));
+// here you tell InteractJS which class of items you'd like to be draggable
+interact('.draggable')
+  .draggable({
+    listeners: {
+      // when a user drags the object ('on dragmove') run this function
+      move: dragMoveListener,
+      // call this function on every dragend event
+      end (event) {
+        console.log("item dropped!")
+      }
+    }
+  })
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+function dragMoveListener (event) {
+  // all this stuff gets the position of the item while it's being dragged
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+  // update the position attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
 }
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
