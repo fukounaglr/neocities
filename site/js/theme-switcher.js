817ef07a5bc0f139879046e12fac109da2346655
ashhmillr <lampoviyk@gmail.com>
2024-06-12 22:24:58 +0300

    update theme system

--- site/js/theme-switcher.js
+++ site/js/theme-switcher.js
@@ -1,10 +1,10 @@
-// stolen from https://codepen.io/troy-sucks/pen/RwOeOEJ
-
-const themes = ["underwater", "green"];
+// Themes: underwater, green
 
 document.body.className = localStorage.getItem("theme");
 
-document.getElementById("toggle").onclick = () => {
-  localStorage.setItem("theme", themes[(themes.indexOf(localStorage.getItem("theme")) + 1) % themes.length]);
-  document.body.className = localStorage.getItem("theme");
-};
+    function changeTheme() {
+        var selectedTheme = document.getElementById('themeSwitcher').value;
+        document.documentElement.setAttribute('theme', selectedTheme);
+        localStorage.setItem('selectedTheme', selectedTheme);
+    }
+
