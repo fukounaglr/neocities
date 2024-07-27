63809b9a1b093f7e236c6c65d23d27b36b3a7c74
ashhmillr <lampoviyk@gmail.com>
2024-07-27 19:50:17 +0300

    come on

--- site/js/theme-switcher.js
+++ site/js/theme-switcher.js
@@ -7,5 +7,5 @@ document.body.className = localStorage.getItem("theme");
   function changeTheme() {
         var selectedTheme = document.getElementById('themeSwitcher').value;
         document.documentElement.setAttribute('theme', selectedTheme);
-        localStorage.setItem("theme", selectedTheme);
+        localStorage.setItem('selectedTheme', selectedTheme);
     }
