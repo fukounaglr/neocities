c701a0ab30486aa45e32bd47edd7d661633e81a6
ashhmillr <lampoviyk@gmail.com>
2024-07-27 15:39:35 +0300

    o

--- site/js/theme-switcher.js
+++ site/js/theme-switcher.js
@@ -4,8 +4,8 @@
 document.body.className = localStorage.getItem("theme");
 
 
-function changeTheme() {
-    var selectedTheme = document.getElementById('themeSwitcher').value;
-    document.documentElement.setAttribute('theme', selectedTheme);
-    localStorage.setItem('selectedTheme', selectedTheme);
-}
+    function changeTheme() {
+        var selectedTheme = document.getElementById('themeSwitcher').value;
+        document.documentElement.setAttribute('theme', selectedTheme);
+        localStorage.setItem('selectedTheme', selectedTheme);
+    }
