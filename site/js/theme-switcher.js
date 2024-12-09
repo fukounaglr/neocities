f00136bed6c6bbe62974fce8c63faba0bf500e82
ashhmillr <lampoviyk@gmail.com>
2024-12-09 19:38:00 +0300

    a few small changes

--- site/js/theme-switcher.js
+++ site/js/theme-switcher.js
@@ -1,6 +1,7 @@
-// const Themes = ["underwater", "green"];
+ const Themes = ["underwater", "green", "neon", "hell", "default"];
 
 document.documentElement.setAttribute('theme', localStorage.getItem("selectedTheme"));
+document.getElementById('themeSwitcher').value = localStorage.getItem("selectedTheme");
 
     function changeTheme() {
         var selectedTheme = document.getElementById('themeSwitcher').value;
