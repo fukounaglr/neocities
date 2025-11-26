3ea433c523c26d6f68bf0d196ea850208accec54
ashhmillr <lampoviyk@gmail.com>
2025-11-26 08:06:19 +0300

    room is back babyyy

--- site/js/talkingfuko.js
+++ site/js/talkingfuko.js
@@ -1,42 +1,43 @@
 var speed = 50;
 const box = document.getElementById("rpgtext");
-const imgIdle = document.getElementById("rpgimgidle");
-const imgTalk = document.getElementById("rpgimgtalk");
+//const imgIdle = document.getElementById("rpgimgidle");
+//const imgTalk = document.getElementById("rpgimgtalk");
 let typingId = 0;
 
-const quote = [
-            "Dimly lit, the room emits a hollow, muffled sound. It houses a single sorry soul, no other to be found",
-            "Water-damaged off-white ceilings, crumbling crown molding, faded paint and threadbare linens and there's no place I'd rather sleep",
-            "These four walls extend beyond my sight and breathe new life into me",
-            "I’ve found my home inside these walls, the world is my own. I write the rules alone in here. I sit atop a throne",
-            "I get this feeling that I’m not the first and not the last to find solace in this empty room",
+const lyric = [
+            "Dimly lit, the room emits a hollow, muffled sound.     It houses a single sorry soul,      no other to be found",
+            "Water-damaged off-white ceilings,      crumbling crown molding,        faded paint and threadbare linens       and there's no place I'd rather sleep",
+            "These four walls extend beyond my sight         and breathe new life into me",
+            "I’ve found my home inside these walls,         the world is my own.                I write the rules alone in here.            I sit atop a throne",
+            "I get this feeling that        I’m not the first and not the last      to find solace in this empty room",
             "These decaying walls were all that kept me sane",
             "不幸な girl, 不幸な girl;        If you wanna flay me let's see what's inside",
-            "The screams of a tiny girl are of no concern to the whole wide world, no matter how loud",
-            "well, this decease is defined by its treatment; you people make me sick!",
-            "I wanna contribute to the chaos, I don't wanna watch and then complain",
+            "The screams of a tiny girl are of no concern to the whole wide world,          no matter how loud",
+            "I wanna contribute to the chaos,       I don't wanna watch and then complain",
             "In my little space     nothing changes.        I don't like this place.        I wanna escape this",
-    "not a sibgle star graces our sky tonight.",
-    "Make sure you kiss your knuckles before you punch me in the face!"
+        "pills in the morning, sleep til the evening    -      life is a dream!     id rather let time march on without me",
+    "let's all give it up for loneliness,       to all the better friends you wish you had instead,     to all i never should have said",
+    "i want to be a computer program,       i want to marry a vocaloid,     i'm just a series of ones and zeros,         the fact that i'm not makes me so annoyed",
+    "lay my head on a bed of nails;     i'm not scared, i'm lonely;     i'm not here    i only long for advancing myself      standing right by hell "
         ];
 const talk = [
-            "Being mentally ill is like being on hard drugs, but with no fun.",
             "Hell is other people.",
-            "i gave up on trying to be okay and now im doing many drugs",
-            "guys i'm so sorry i was toxic that won't happen again [turns into a biblically accurate angel]",
-            "my hate club is so small that i am the one who has to do the all hating",
-            "what doesn't kill me should try harder",
+            "my hate club is so small,      that i am the one who has to do the all hating",
+            "what doesn't kill me       should try harder",
             "i have 0 knowledge",
-            "gay bloodcells be carrying homoglobin",
             "tasty megabites...",
             "if there's adverb and verb, there should be adjective and jective",
-    "stereo sound makes my head hurt..."
+    "stereo sound makes my head hurt...",
+    "Is [Creator] ever gonna come back?",
+    "I hope that red haired kid burns in hell...",
         ];
 const about = [
-    "hi! i'm this site's mascot.",
     "My name is Fuko Una!",
-    "Fuko 2.0!",
-    "beep boop, i'm  a robot",
+    "Beep boop, i'm  a robot!",
+    "I'm a prototype. The software never got to release. But that means I'm special! One of a kind!",
+    "Your personal digital assistant! I will help you with tasks and keep you company!",
+    "You don't need anyone elsse when you've got a friend like me!",
+    "I can think, I can feel. Isn't that what makes you real?",
     "Powered by meowclawsoft :3",
     "I live in your cat-puter!",
     "you are just as much a tool as i am!",
@@ -60,12 +61,18 @@ const joke = [
     "a car carreered? how much did it get paid?",
     "ppl in stone age going to sleep be like: ah yes, bed rock",
     "how do you call a sleeping male cow? a bulldozer.",
-    "fungi? what's so fun about him?"
+    "fungi? what's so fun about him?",
+    "gay bloodcells be carrying homoglobin"
         ];
 
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+box.innerHTML = "* ";
+txt = "Hi! I'm Fuko Una -- mascot of this site! You can click on buttons below for me to talk. Click a second time to skip!";
+typeWriter(0);
+
 async function speech(a) {
     typingId++;
     const randomIndex = Math.floor(Math.random() * a.length);
@@ -74,15 +81,15 @@ async function speech(a) {
 
     typeWriter(typingId);
 
-    await sleep(speed * txt.length * 1.2);
-    imgIdle.style.display = "block";
-    imgTalk.style.display = "none";
+    //await sleep(speed * txt.length * 1.2);
+    //imgIdle.style.display = "block";
+    //imgTalk.style.display = "none";
 }
 
 async function typeWriter(myId) {
     for (let i = 0; i < txt.length; i++) {
-        imgIdle.style.display = "none";
-        imgTalk.style.display = "block";
+        //imgIdle.style.display = "none";
+        //imgTalk.style.display = "block";
         if (myId !== typingId) return;
         console.log(i);
         box.innerHTML += txt.charAt(i);
